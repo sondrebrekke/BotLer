6a3c99eeafbd83186181a375a84ce99aa885731b
jorboer <jorgfb@stud.ntnu.no>
2017-03-01 15:20:58 +0100

    Feedback Subject

---
+++ BotLer - Prototype 1/FeedbackSubject.swift
@@ -0,0 +1,35 @@
+//
+//  FeedbackSubject.swift
+//  BotLer - Prototype 1
+//
+//  Created by Jørgen Frost Bø on 01.03.2017.
+//  Copyright © 2017 Sondre Brekke. All rights reserved.
+//
+
+import UIKit
+
+class FeedbackSubject: ViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print(ChooseSubject.mineFag)
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
