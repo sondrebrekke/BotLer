8cbc35b9a558d174016b64848d824bd18032589c
Sondre Brekke <sondrbre@stud.ntnu.no>
2017-03-01 12:10:39 +0100

    Fixed html and php

--- Webpage/endreinfo.php
+++ Webpage/endreinfo.php
@@ -1,5 +1,5 @@
 <?php
-	$conn = new mysqli('mysql.stud.ntnu.no', 'jorgfb_botler', 'park12', '14sondre'); //Her kobler jeg meg til området mitt på nvselev.no. Jeg skriver inn server, brukernavn, passord og databsenavnet.
+	$conn = new mysqli('mysql.stud.ntnu.no', 'jorgfb_botler', 'park12', 'jorgfb_botler_database'); //Her kobler jeg meg til området mitt på nvselev.no. Jeg skriver inn server, brukernavn, passord og databsenavnet.
 	if (!mysqli_set_charset($conn, "utf8")) { //Her sier jeg at hvis ikke tegnsettingen er satt til UTF-8, skal den "skrive ut" på siden Feil ved lasting av tegnsettet utf8. Brukeren vil også få en feilmelding.
 		printf("Feil ved lasting av tegnsettet utf8: %s\n", mysqli_error($conn));
 		printf('<BR>');
@@ -13,7 +13,7 @@
 		$brukernavn = trim($_POST['brukernavn']); //Skal den trimme og legge $_POST brukernavn inn i variabelen $brukernavn slik at den informasjonen brukeren skrev inn enkelt kan hentes ut. 
 		$passord = trim($_POST['passord']);	//Skal den trimme og legge $_POST passord inn i variabelen $passord slik at den informasjonen brukeren skrev inn enkelt kan hentes ut. 
 				
-		if($results = $conn->query("SELECT * FROM prosjekt_info WHERE brukernavn= '$brukernavn' AND passord= '$passord'")) //Hvis resultatene stemmer med en bruker fra databasen min og tabellen prosjekt_info, skal den hente ut alle feltene. 
+		if($results = $conn->query("SELECT * FROM lecturer WHERE username= '$username' AND password= '$password'")) //Hvis resultatene stemmer med en bruker fra databasen min og tabellen prosjekt_info, skal den hente ut alle feltene. 
 		{
 			if($results->num_rows) { //hvis det er noen rader som stemmer
 				while($row = $results->fetch_object()) { //Gjør klar og henter ut informasjonen
@@ -28,3 +28,10 @@
 		
 	}
 ?>
+<!DOCTYPE html>
+<html>
+
+<h1>Du er logget inn.</h1>
+
+</html>
+
---
+++ Webpage/feillogin.php
@@ -0,0 +1,6 @@
+<!DOCTYPE html>
+<html>
+
+<h1>Du er IKKE logget inn.</h1>
+
+</html>
